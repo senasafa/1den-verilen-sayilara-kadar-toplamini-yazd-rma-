c1851ed9105c975cc2101a547bfe652dca147e9e
senasafa <senasafa8@gmail.com>
2024-11-21 00:33:40 +0300

    Add files via upload

---
+++ 1den verilen sayılara kadar toplamını yazdırma.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+        Console.WriteLine("bir sayı giriniz: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+
+        int toplam = 0;
+
+        for (int i = 1; i < n; i++);
+
+        {
+            int i = 1;
+            toplam += i;
+
+            Console.WriteLine("toplam");
+            Console.WriteLine(toplam);
+
+
+
+
+
+
+          }
+
+
+
+          
